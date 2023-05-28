f18e315fe1131a97a285efca5e5020df4df93baa
Aelita Jelden <nyc.aelita@gmail.com>
2023-05-29 03:48:48 +0600

    Initial commit for selenium

---
+++ src/test/java/com/automation/DemoClass.java
@@ -0,0 +1,22 @@
+package com.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoClass {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.saucedemo.com/");
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        username.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+        loginBtn.click();
+
+    }
+}
---
+++ src/test/java/com/automation/DemoClass3.java
@@ -0,0 +1,34 @@
+package com.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class DemoClass3 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+
+        driver.get("http://www.ebay.com/");
+
+
+      WebElement search = driver.findElement(By.xpath("//input[@id='gh-ac']"));
+      WebElement searchBtn = driver.findElement(By.xpath("//input[@id='gh-btn']"));
+      search.sendKeys("laptop");
+      searchBtn.click();
+
+
+      List<WebElement> listOfPrice = driver.findElements(By.xpath("//span[@class='s-item__price']"));
+
+      for (WebElement price : listOfPrice) {
+          System.out.println(price.getText());
+      }
+
+
+    }
+}
